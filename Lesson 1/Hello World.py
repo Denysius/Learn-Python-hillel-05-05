7420d136b1818761ed8146d1956ac42a897d1693
Denysius <Denis4Broker@gmail.com>
2023-05-09 16:01:27 +0300

    Committing first files

--- Lesson 1/Hello World.py
+++ Lesson 1/Hello World.py
@@ -1,1 +1,14 @@
+print(5)
 
+print('Hello World')
+print("Hello World")
+
+x=69
+print(x)
+
+name = input('What is your name?')
+
+print('Hello, name')
+print('Hello, ' , name)
+print(f'Hello, {name}')
+print(f"Hello, {name}")
