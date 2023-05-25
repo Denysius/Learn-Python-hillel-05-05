8fa48996bbaa5b1738162e9033c8c5a55b34fce7
Denysius <Denis4Broker@gmail.com>
2023-05-26 00:29:55 +0300

    Committing first files

---
+++ Homework lesson_2 (15.05.2023)/Domashka 4/Domashka 4.py
@@ -0,0 +1,21 @@
+sum_key = "sum"
+i = []
+
+while True:
+    x = input("Введіть число або 'sum' для отримання суми: ")
+
+    if x.isdigit() or (x[0] == '-' and x[1:].isdigit()):
+        y = float(x)
+        i.append(y)
+    else:
+        print("Некоректне введення! Введіть число або 'sum'.")
+
+    if x.lower() == sum_key:
+        break
+
+if len(i) > 0:
+    result = sum(i)
+    print("Сума введених чисел: ", result)
+
+else:
+    print("Жодного числа не було введено.")
