63b6396844182b16ef1b9126d21e5d261068d796
Denysius <Denis4Broker@gmail.com>
2023-06-02 18:13:34 +0300

    Committing first files

--- Homework lesson_2 (15.05.2023)/Homework 5/Homework 5.2.py
+++ Homework lesson_2 (15.05.2023)/Homework 5/Homework 5.2.py
@@ -1,11 +1,14 @@
-print("Доброго вечора! Введіть свій текст на редагування:\n")
+print("Доброго вечора! Введіть свій текст на редагування (або введіть 'exit' для виходу):\n")
 x = input()
-# Задаємо умову для пошуку індексів дужок на відкриття та закриття
-open_brackets = x.find("(")
-close_brackets = x.find(")")
-# Використовуємо слайсінг для видалення тексту в дужках
-if open_brackets != -1 and close_brackets != -1:
-    Redact = x[:open_brackets] + x[close_brackets+1:]
-else:
-    Redact = x
-print(Redact)
+# Задаємо умову для пошуку індексів дужок на відкриття та закриття а також на вихід
+while x != "exit":
+    open_brackets = x.find("(")
+    close_brackets = x.find(")")
+# Використовуємо слайсінг для видалення тексту в дужках і продовжуємо цикл
+    if open_brackets != -1 and close_brackets != -1:
+        x = x[:open_brackets] + x[close_brackets+1:]
+        continue
+    print("Відредагований текст:", x)
+
+    print("Введіть свій текст на редагування (або введіть 'exit' для виходу):")
+    x = input()
