c85dc712f76228fdf2d4fb1718d249a30473be14
Denysius <Denis4Broker@gmail.com>
2023-06-08 15:43:27 +0300

    Committing first files

--- Homework 6/Homework6.1.py
+++ Homework 6/Homework6.1.py
@@ -2,18 +2,12 @@ def remove_brackets(user_text):
     while True:
         open_brackets = user_text.find("(")
         close_brackets = user_text.find(")")
-        # print(open_brackets)
-        # print(close_brackets)
-        # print(open_brackets != -1 and close_brackets != -1)
+
         if open_brackets != -1 and close_brackets != -1:
-            print('logic works')
             user_text = user_text[:open_brackets] + user_text[close_brackets + 1:]
         else:
             break
 
-
-        # user_text = input("Введіть свій текст на редагування (або введіть 'exit' для виходу): ")
-        # return user_text
     print("Відредагований текст:", user_text)
 
 
@@ -22,5 +16,4 @@ while user_input != "exit":
     remove_brackets(user_input)
     user_input = input("Введіть свій текст на редагування (або введіть 'exit' для виходу): ")
 else:
-    print('you exited')
-
+    print('Ви вийшли')
