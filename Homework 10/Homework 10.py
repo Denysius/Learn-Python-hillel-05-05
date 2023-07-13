04e9fcdf11639a92ef8ec053c341a6ae775e5c2a
Denysius <Denis4Broker@gmail.com>
2023-07-13 20:55:32 +0300

    Committing first files

--- Homework 10/Homework 10.py
+++ Homework 10/Homework 10.py
@@ -5,6 +5,15 @@ def read_tech_inventory(file_path):
     category_index = {}
     brand_index = {}
 
+    # Генеруємо унікальний ID
+    def generate_unique_id():
+        id_counter = 1
+        while True:
+            yield id_counter
+            id_counter += 1
+
+    unique_id_generator = generate_unique_id()
+
     with open(file_path, 'r', newline='') as file:
         reader = csv.DictReader(file)
         for row in reader:
@@ -13,19 +22,17 @@ def read_tech_inventory(file_path):
             brand = row['brand']
             price = float(row['price'])
 
-            # Generate unique ID for each item
-            item_id = len(tech_data) + 1
-
-            # Add item to tech_data dictionary
+            # Генеруємо унікальний ID кожної одиниці
+            item_id = next(unique_id_generator)
             tech_data[item_id] = {'model': model, 'category': category, 'brand': brand, 'price': price}
 
-            # Update category index
+            # Оновлення індексу категорії
             if category in category_index:
                 category_index[category].append(item_id)
             else:
                 category_index[category] = [item_id]
 
-            # Update brand index
+            # Оновлення індексу бренду
             if brand in brand_index:
                 brand_index[brand].append(item_id)
             else:
@@ -35,17 +42,17 @@ def read_tech_inventory(file_path):
 
 
 def print_brand_category_statistics(tech_data, category_index, brand_index):
-    print("Brand Statistics:")
+    print("Статистика брендів:")
     for brand, items in brand_index.items():
         print(f"{brand}: {len(items)} items")
 
-    print("\nCategory Statistics:")
+    print("\nСтатистика категорії:")
     for category, items in category_index.items():
         print(f"{category}: {len(items)} items")
 
 
 def print_items_by_brand_category(tech_data, category_index, brand_index, selected_category, selected_brand):
-    print(f"\nItems of Brand: {selected_brand} | Category: {selected_category}")
+    print(f"\nБренд: {selected_brand} | Категорія: {selected_category}")
     for item_id in category_index[selected_category]:
         item = tech_data[item_id]
         if item['brand'] == selected_brand:
@@ -68,23 +75,47 @@ def calculate_brand_distribution_by_category(tech_data, category_index, brand_in
 
 
 def print_brand_distribution(brand_distribution):
-    print("\nBrand Distribution by Category:")
+    print("\nРозподіл продукції за категоріями:")
     for category, brands in brand_distribution.items():
         print(category)
         for brand, count in brands.items():
             print(f"{brand}: {count} items")
         print()
 
-# Запуск програми
-file_path = 'tech_inventory.csv'
-tech_data, category_index, brand_index = read_tech_inventory(file_path)
-print_brand_category_statistics(tech_data, category_index, brand_index)
 
-# Обрані бренд та категорія для виводу
-selected_category = 'Ноутбуки'
-selected_brand = 'Lenovo'
-print_items_by_brand_category(tech_data, category_index, brand_index, selected_category, selected_brand)
+if __name__ == '__main__':
+    # Запуск програми
+    file_path = 'tech_inventory.csv'
+    tech_data, category_index, brand_index = read_tech_inventory(file_path)
 
-# Розподіл товарів по брендам для кожної категорії
-brand_distribution = calculate_brand_distribution_by_category(tech_data, category_index, brand_index)
-print_brand_distribution(brand_distribution)
+    # Виводимо список брендів
+    print("Доступні бренди:")
+    for brand in brand_index.keys():
+        print(brand)
+
+    # Виводимо список категорій
+    print("\nДоступні категорії:")
+    for category in category_index.keys():
+        print(category)
+
+    while True:
+        print("\nКоманди:")
+        print("1. Натисніть 1 для виводу статистики")
+        print("2. Натисніть 2 для вибіркового виводу повної інформації товару по бренду та категорії")
+        print("3. Натисьніть 3 для розподілу товарів по брендам для кожної категорії")
+        print("4. Натисьніть 4 для виходу")
+        choice = input("Введіть номер команди для продовження: ")
+
+        if choice == '1':
+            print_brand_category_statistics(tech_data, category_index, brand_index)
+        elif choice == '2':
+            selected_category = input("Введіть назву категорії з урахуванням регістру: ")
+            selected_brand = input("Введіть назву бренду з урахуванням регістру: ")
+            print_items_by_brand_category(tech_data, category_index, brand_index, selected_category, selected_brand)
+        elif choice == '3':
+            brand_distribution = calculate_brand_distribution_by_category(tech_data, category_index, brand_index)
+            print_brand_distribution(brand_distribution)
+        elif choice == '4':
+            break
+        else:
+            print("Помилка, спробуйте знов.")
